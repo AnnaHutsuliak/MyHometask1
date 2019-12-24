55e47d75f61b68b24c0ed0c5d9452a72dd09661e
AnnaHutsuliak <59147033+AnnaHutsuliak@users.noreply.github.com>
2019-12-24 23:05:29 +0200

    Updated because of the class
    Updated because of the class should be printed with the capital letter

--- com/epam/hometask1/hometask1.java
+++ com/epam/hometask1/hometask1.java
@@ -1,6 +1,6 @@
 package com.epam.hometask1;
 
-public class hometask1 {
+public class Hometask1 {
     public static void main (String [] arg) {
         System.out.println("****   ****      *       *       *");
         System.out.println("*      *  *     * *      * *   * *");
