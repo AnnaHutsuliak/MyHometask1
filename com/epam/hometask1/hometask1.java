331280c97fbe20c9064405501cc20f75f447e4d7
Anna Hutsuliak <anhutsuliak@gmail.com>
2019-12-24 22:07:18 +0200

    first homework

---
+++ com/epam/hometask1/hometask1.java
@@ -0,0 +1,12 @@
+package com.epam.hometask1;
+
+public class hometask1 {
+    public static void main (String [] arg) {
+        System.out.println("****   ****      *       *       *");
+        System.out.println("*      *  *     * *      * *   * *");
+        System.out.println("**     ****    *   *     *   *   *");
+        System.out.println("*      *      *******    *       *");
+        System.out.println("****   *     *       *   *       * ");
+
+    }
+}
